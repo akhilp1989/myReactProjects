7ee50dc5c1c6d4e87097676e9ef5a84b6efd41db
akhilp1989 <akhilp1989@users.noreply.github.com>
2021-07-20 20:44:32 -0700

    Feature 3 (#6)
    * After rebasing
    
    * After rebase
    
    Co-authored-by: akhil.parashara <akhil.parashara@servicenow.com>

--- netflixreact/src/App.js
+++ netflixreact/src/App.js
@@ -3,7 +3,7 @@ import Jumbotron from './Components/Jumbotron'
 import jumboData from './fixtures/jumbo.json'
 
 function App() {
-  return <div><p>Hello-1</p><p>Hello 2</p></div>
+  return <div><p>Hello 4.2</p><p>Hello 2</p></div>
 }
 
 export default App;
