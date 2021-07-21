b1dc040b2b3d3c0cf172d64fe88525484bbb80bb
akhil.parashara <akhil.parashara@servicenow.com>
2021-07-20 21:27:03 -0700

    Updated merge confilict.1

--- netflixreact/src/App.js
+++ netflixreact/src/App.js
@@ -3,7 +3,7 @@ import Jumbotron from './Components/Jumbotron'
 import jumboData from './fixtures/jumbo.json'
 
 function App() {
-  return <div><p>Hello from feature 5 local</p><p>Hello 2</p></div>
+  return <div><p>Hello from feature 5</p><p>Hello 2</p></div>
 }
 
 export default App;
